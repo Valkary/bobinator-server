6b877930975684ad0c9d6bf02d97aca02efe2d6e
Valkary <pepoclesng@gmail.com>
2022-06-08 12:50:39 -0500

    Arreglando un problema con la cola de produccion

--- src/functions/prod/updateGlobalProdState.ts
+++ src/functions/prod/updateGlobalProdState.ts
@@ -1,9 +1,10 @@
 import pool_connection from "../connection";
 
 export function updateGlobalProdState(prod_id: number, curr_order: number) {
+  console.log("Updating!");
   return new Promise((resolve, reject) => {
     pool_connection.query(
-      `UPDATE global_state SET state = ?, curr_order = ? WHERE state = 1;`,
+      `UPDATE global_state SET state = ?, curr_order = ? WHERE state = 1 OR state = 2 OR state = 3;`,
       [prod_id, curr_order],
       (err, result) => {
         if (err) reject(err);
